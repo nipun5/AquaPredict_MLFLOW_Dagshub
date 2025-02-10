6e285f628f6ab1c7e4549fb6cd238eb647efd10c
Nipun Jain <jainnipun891@gmail.com>
2025-02-10 16:18:28 +0530

    Added water_train_rf.py

---
+++ water_train_rf.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import mlflow
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+import dagshub
+dagshub.init(repo_owner='nipun5', repo_name='AquaPredict_MLFLOW_Dagshub', mlflow=True)
+
+
+mlflow.set_experiment("water_exp1")
+mlflow.set_tracking_uri("https://dagshub.com/nipun5/AquaPredict_MLFLOW_Dagshub.mlflow")
+data = pd.read_csv("D:/EXP_TRACKING_DVC/data/water_potability.csv")
+
+from sklearn.model_selection import train_test_split
+train_data,test_data = train_test_split(data,test_size=0.20,random_state=42)
+
+def fill_missing_with_median(df):
+    for column in df.columns:
+        if df[column].isnull().any():
+            median_value = df[column].median()
+            df[column].fillna(median_value,inplace=True)
+    return df
+
+
+# Fill missing values with median
+train_processed_data = fill_missing_with_median(train_data)
+test_processed_data = fill_missing_with_median(test_data)
+
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+X_train = train_processed_data.iloc[:,0:-1].values
+y_train = train_processed_data.iloc[:,-1].values
+
+n_estimators = 500
+with mlflow.start_run():
+    clf = RandomForestClassifier(n_estimators=n_estimators)
+    clf.fit(X_train,y_train)
+
+    # save 
+    pickle.dump(clf,open("model.pkl","wb"))
+
+    X_test = test_processed_data.iloc[:,0:-1].values
+    y_test = test_processed_data.iloc[:,-1].values
+
+    from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+
+    model = pickle.load(open('model.pkl',"rb"))
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test,y_pred)
+    precision = precision_score(y_test,y_pred)
+    recall = recall_score(y_test,y_pred)
+    f1_score = f1_score(y_test,y_pred)
+
+    mlflow.log_metric("acc",acc)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1-score",f1_score)
+
+    mlflow.log_param("n_estimators",n_estimators)
+
+    cm = confusion_matrix(y_test,y_pred)
+    plt.figure(figsize=(5,5))
+    sns.heatmap(cm,annot=True,fmt='d')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Confusion Matrix')
+    plt.savefig("confusion_matrix.png")
+
+    mlflow.log_artifact("confusion_matrix.png")
+    mlflow.sklearn.log_model(clf,"RandomForestClassifier")
+
+    mlflow.log_artifact(__file__)
+
+    mlflow.set_tag("author","AquaPredict")
+    mlflow.set_tag("model","RF")
+
+    print("acc",acc)
+    print("precision", precision)
+    print("recall", recall)
+    print("f1-score",f1_score)
